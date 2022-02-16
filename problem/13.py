4c9a86aa757a1d31c10a695bc92773d460198c90
vivek-437 <92535088+vivek-437@users.noreply.github.com>
2022-02-17 00:12:52 +0530

    Add files via upload

---
+++ problem/13.py
@@ -0,0 +1,20 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    x=student_marks[query_name]
+    totalnum=0
+    for i in x:
+        totalnum+=i
+    len()
+    print ("%.2f" % round(totalnum/len(x),2)) 
+
+# 3
+# Krishna 67 68 69
+# Arjun 70 98 63
+# Malika 52 56 60
+# Malika
